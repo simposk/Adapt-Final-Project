da66f4fefa96b8756b7f8147050f3077f85872cd
Justas Kučinskas <justkucinskas@gmail.com>
2018-12-16 18:35:08 +0200

    added slider

--- src/components/Home/SliderBox.js
+++ src/components/Home/SliderBox.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import paragraphImg from '../../assets/wireframes/paragraph.png';
-
+import Slider from './Slider';
 import Box from '../base/Box';
+import { Link } from 'react-router-dom';
+const slides = [
+  {
+    city: 'Paris',
+    country: 'France',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/paris.jpg',
+  },
+  {
+    city: 'Singapore',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/singapore.jpg',
+  },
+  {
+    city: 'Prague',
+    country: 'Czech Republic',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/prague.jpg',
+  },
+  {
+    city: 'Amsterdam',
+    country: 'Netherlands',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/amsterdam.jpg',
+  },
+  {
+    city: 'Moscow',
+    country: 'Russia',
+    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/moscow.jpg',
+  },
+];
 
 const SliderBox = () => (
   <Box>
@@ -9,10 +36,11 @@ const SliderBox = () => (
       <h3>Slider</h3>
     </div>
     <div className="divider margin--small-bottom" />
-    <img
-      className="home__wireframe-img"
-      src={ paragraphImg }
-    />
+
+    <div>
+    <Slider slides={ slides }></Slider>
+    </div>
+    
   </Box>
 );
 
