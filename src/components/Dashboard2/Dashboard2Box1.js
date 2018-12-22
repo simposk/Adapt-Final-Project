495598fb9565f0698b14c3f13d375668c1fe0314
Justas Kučinskas <justkucinskas@gmail.com>
2018-12-22 23:49:59 +0200

    niekas neveikia
    aciu

--- src/components/Dashboard2/Dashboard2Box1.js
+++ src/components/Dashboard2/Dashboard2Box1.js
@@ -1,106 +1,178 @@
 /* eslint-disable */
-import React, { Component } from 'react';
-import Box from '../base/Box';
-import paragraphImg from '../../assets/wireframes/paragraph.png';
+import React, { Component } from "react";
+import Box from "../base/Box";
+import paragraphImg from "../../assets/wireframes/paragraph.png";
 // import Chart from './Chart';
-import { LineChart } from 'react-charts-d3';
-import Axios from 'axios';
+import { LineChart } from "react-easy-chart";
+import Axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import _ from 'lodash';
+import _ from "lodash";
 
 class Dashboard2Box1 extends Component {
-  constructor(){
+  constructor() {
     super();
 
-    let today = new Date();
-    const nuo = today.getFullYear() + "." + parseInt(today.getMonth() + 1) + "." +  parseInt(today.getDate() + 1);
-    let todayUnix = new Date(nuo).getTime() / 1000;
+    let today = Date.now();
+    // const nuo =
+    //   today.getFullYear() +
+    //   "." +
+    //   parseInt(today.getMonth() + 1) +
+    //   "." +
+    //   parseInt(today.getDate() + 1);
+    // let todayUnix = new Date(nuo).getTime() / 1000;
 
     this.state = {
       values: [],
       startDate: new Date(),
       endDate: new Date(),
-      unixFrom: todayUnix,
-      unixTo: todayUnix,
-    } 
+      unixFrom: today,
+      unixTo: today,
+      today: today,
+    };
   }
 
-
-
-  getChartData = () => {
-
-  }
-
+  getChartData = () => {};
 
   handleChange = date => {
-
     const demo = new Date(date);
-    const nuo = demo.getFullYear() + "." + parseInt(demo.getMonth() + 1) + "." +  parseInt(demo.getDate() + 1);
+    const nuo =
+      demo.getFullYear() +
+      "." +
+      parseInt(demo.getMonth() + 1) +
+      "." +
+      parseInt(demo.getDate() + 1);
     var unixFrom = new Date(nuo).getTime() / 1000;
 
     let { unixTo, endDate } = this.state;
 
-    if(unixFrom < unixTo) {
-        this.setState({
-            startDate: date,
-            unixFrom
-        })
-    } else {
-        this.setState({
-            startDate: endDate,
-        }) 
+    if (unixFrom > this.state.today) {
+      this.setState({
+        startDate: this.state.today,
+        unixFrom: this.state.today,
+      })
     }
-}
+    else if (unixFrom < unixTo) {
+      this.setState({
+        startDate: date,
+        unixFrom
+      });
+    }
+    else {
+      this.setState({
+        startDate: endDate
+      });
+    }
+  };
 
-handleChange2 = date => {
+  handleChange2 = date => {
     const demo = new Date(date);
-    const nuo = demo.getFullYear() + "." + parseInt(demo.getMonth() + 1) + "." +  parseInt(demo.getDate() + 1);
+    const nuo =
+      demo.getFullYear() +
+      "." +
+      parseInt(demo.getMonth() + 1) +
+      "." +
+      parseInt(demo.getDate() + 1);
     var unixTo = new Date(nuo).getTime() / 1000;
 
     const { unixFrom, startDate } = this.state;
 
     if (unixFrom < unixTo) {
-        this.setState({
-            endDate: date,
-            unixTo
-        })    
-    } else {
-        this.setState({
-            endDate: startDate,
-        }) 
+      this.setState({
+        endDate: this.state.today,
+        unixTo: this.state.today
+      })
+    } 
+    else if (unixTo > this.state.today)
+    {
+      this.setState({
+        endDate: date,
+        unixTo
+      });
     }
-}
+    else {
+      this.setState({
+        endDate: startDate
+      });
+    }
+  };
 
-handleDateSearch = async () => {
+  handleDateSearch = async () => {
     const { unixTo, startDate, endDate } = this.state;
 
     var days = Math.floor(Math.abs(endDate - startDate) / 1000 / 86400);
 
-    const apiEndpoint = "https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=" + days + "&toTs=" + unixTo;
+    const apiEndpoint =
+      "https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=" +
+      days +
+      "&toTs=" +
+      unixTo;
 
     const { data } = await Axios.get(apiEndpoint);
 
     let values = _.values(data.Data);
 
+//     // Create a new JavaScript Date object based on the timestamp
+// // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+// var date = new Date(unix_timestamp*1000);
+// // Hours part from the timestamp
+// var hours = date.getHours();
+// // Minutes part from the timestamp
+// var minutes = "0" + date.getMinutes();
+// // Seconds part from the timestamp
+// var seconds = "0" + date.getSeconds();
+
+// // Will display time in 10:30:23 format
+// var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+
     let lineValues = _.map(values, item => {
-        return {x: item.time, y: item.close}
+      let date = new Date(item.time * 1000).toISOString().substring(0, 10);
+      //WARNING! To be deprecated in React v17. Use componentDidMount instead.
+      console.log(date);
+      return { x: date, y: item.close };
     });
-
-    let arrayOfLineValueObjects = [...lineValues];
-
-    let lineChartData = {};
-    lineChartData.key = 'Group 1';
-    lineChartData.values = arrayOfLineValueObjects;
-
-    console.log(lineChartData);
 
     this.setState({
-        values: lineChartData,
+      values: [lineValues]
     });
-}
+  };
+
+  mouseOverHandler = (d, e) => {
+    this.setState({
+      showToolTip: true,
+      top: `${e.screenY - 10}px`,
+      left: `${e.screenX + 10}px`,
+      y: d.y,
+      x: d.x});
+  }
+
+  mouseMoveHandler = (e) => {
+    if (this.state.showToolTip) {
+      this.setState({top: `${e.y - 10}px`, left: `${e.x + 10}px`});
+    }
+  }
+
+  mouseOutHandler = () => {
+    this.setState({showToolTip: false});
+  }
+
+  createTooltip = () => {
+    if (this.state.showToolTip) {
+      return (
+        <ToolTip
+          top={this.state.top}
+          left={this.state.left}
+        >
+            The x value is {this.state.x} and the y value is {this.state.y}
+        </ToolTip>
+      );
+    }
+    return false;
+  }
 
   render() {
+    console.log(this.state.values);
     return (
       <React.Fragment>
         <Box>
@@ -112,14 +184,27 @@ handleDateSearch = async () => {
             selected={this.state.endDate}
             onChange={this.handleChange2}
           />
-          <input type ="submit" style={{float: 'right'}} value='Update' onClick={ this.handleDateSearch }></input>
+          <input
+            type="submit"
+            style={{ float: "right" }}
+            value="Update"
+            onClick={this.handleDateSearch}
+          />
         </Box>
-        
+
         <Box>
-            <LineChart data = {this.state.values} />
+          <LineChart
+            xType={'text'}
+            axes
+            grid
+            verticalGrid
+            lineColors={['green']}
+            interpolate={'cardinal'}
+            width={450}
+            height={450}
+            data={this.state.values} />
         </Box>
       </React.Fragment>
-      
     );
   }
 }
